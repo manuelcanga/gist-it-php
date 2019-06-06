f7d44831e64e4484a24aa6deeb320137993e1b57
Manuel Canga <dev@trasweb.net>
2019-06-06 19:44:39 +0200

    Feat: add an acceptance test

--- tests/acceptance/EmbedCest.php
+++ tests/acceptance/EmbedCest.php
@@ -7,10 +7,26 @@ class EmbedCest
     {
     }
 
-    // tests
-    public function embed(AcceptanceTester $I)
+    /**
+     * Run: php ../vendor/bin/codecept run --steps from 'tests' directory
+     *
+     * @param AcceptanceTester $I
+     */
+    public function testEmbedExample(AcceptanceTester $I)
     {
         $I->wantTo('generate code embed for Github URL');
-        $I->amOnPage('/trasweb/Team/blob/develop/Team.php');
+        $I->amOnPage('/example.html');
+        $I->canSeeInTitle('Gist-it-php - Example');
+
+        //piece of code
+        $I->cansee('namespace gist_it_php');
+        $I->cansee('const _APP_ = __DIR__;');
+
+        //Relative links
+        $I->canSeeLink('view raw', 'https://github.com/trasweb/gist-it-php/raw/master/index.php');
+        $I->canSeeLink('index.php', 'https://github.com/trasweb/gist-it-php/blob/master/index.php');
+
+        //line numbers in gutter
+        $I->cansee('28', '.gist-gutter');
     }
 }
